40e28219b907a65f8a3b0453088dd3000fb060e0
JesperStroebaek <jesp73g2@stud.kea.dk>
2022-04-21 23:48:26 +0200

    Initial commit

---
+++ src/main/java/com/springmysqlazureproject/SpringMysqlAzureProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springmysqlazureproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlAzureProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMysqlAzureProjectApplication.class, args);
+    }
+
+}
